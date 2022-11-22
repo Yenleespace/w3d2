16ec9d944f2933527c0d6fabb98bff9751da6a4f
Yenleespace <yenny0104@gmail.com>
2022-11-22 18:23:33 -0500

    third

--- game.rb
+++ game.rb
@@ -15,21 +15,23 @@ class Game
             sleep(3)
             system("clear")
             @board.render
+            puts 'Enter a position'
             guessed_pos = gets.chomp.split(" ").map {|ele| ele.to_i}
-
+            raise 'Invalid' if guessed_pos.length != 2 
             make_guess(guessed_pos)
         end
+        puts "win"
     end
 
 
     def make_guess(pos)
         if previous_guess == nil
-            @grid[guessed_pos].reveal
+            @board[guessed_pos].reveal
             previous_guess = guessed_pos
         else
-            unless @grid[guessed_pos] == @grid[previous_guess]
-                @grid[guessed_pos].hide
-                @grid[previous_guess].hide
+            unless @board[guessed_pos] == @board[previous_guess]
+                @board[guessed_pos].hide
+                @board[previous_guess].hide
                 guessed_pos = nil
                 previous_guess = nil
             end
